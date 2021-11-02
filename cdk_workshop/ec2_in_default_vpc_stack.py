3a435ef4b567469783e7bff23bf4dca2f3312cc5
Lin <yenhlin@88665a0e13fc.ant.amazon.com>
2021-11-02 18:24:31 +0800

    removed unnecessary contents

--- cdk_workshop/ec2_in_default_vpc_stack.py
+++ cdk_workshop/ec2_in_default_vpc_stack.py
@@ -3,11 +3,6 @@ from aws_cdk import (
     core
 )
 
-# For consistency with other languages, `cdk` is the preferred import name for
-# the CDK's core module.  The following line also imports it as `core` for use
-# with examples from the CDK Developer's Guide, which are in the process of
-# being updated to use `cdk`.  You may delete this import if you don't need it.
-# vpc = ec2.Vpc.from_lookup(self, "vpc", vpc_id=vpcID)
 
 instanceName = "workshop-ec2-instance"
 linux_ami = ec2.AmazonLinuxImage(generation=ec2.AmazonLinuxGeneration.AMAZON_LINUX,
